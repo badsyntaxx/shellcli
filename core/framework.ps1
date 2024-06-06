cc3833a8893e77c2aa66293ad344d6020ccc0e86
badsyntaxx <badsyntaxx@gmail.com>
2024-06-06 15:37:15 -0600

    test

--- core/framework.ps1
+++ core/framework.ps1
@@ -259,9 +259,9 @@ function write-welcome {
 
     # Get-Item -ErrorAction SilentlyContinue "$env:TEMP\CHASED-Script.ps1" | Remove-Item -ErrorAction SilentlyContinue
     Write-Host
-    Write-Host " :: Executing command:"  -ForegroundColor Cyan -NoNewline
+    Write-Host " :: Executing command:"  -ForegroundColor Gray -NoNewline
     Write-Host " $Command" -ForegroundColor DarkGreen -NoNewline
-    Write-Host " | $Description" -ForegroundColor Cyan
+    Write-Host " | $Description" -ForegroundColor Gray
 }
 
 function get-download {
