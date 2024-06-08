d4ce586c3a73a3b82e6160166f0f034882923b19
badsyntaxx <badsyntaxx@gmail.com>
2024-06-08 15:41:19 -0600

    bug fixes

--- windows/get-wifi-creds.ps1
+++ windows/get-wifi-creds.ps1
@@ -1,16 +1,16 @@
 function get-wifi-creds {
     $wifiProfiles = netsh wlan show profiles
     if ($wifiProfiles -match "There is no wireless interface on the system.") {
-        exit-script -type "error" -text "There is no wireless interface on the system."
+        exit-script -type "error" -text "There is no wireless interface on the system." -lineBefore -lineAfter
     }
 
     if ($wifiProfiles.Count -gt 0) {
         $wifiList = ($wifiProfiles | Select-String -Pattern "\w*All User Profile.*: (.*)" -AllMatches).Matches | ForEach-Object { $_.Groups[1].Value }
     } else {
-        exit-script -type "error" -text "No WiFi profiles found."
+        exit-script -type "error" -text "No WiFi profiles found." -lineBefore -lineAfter
     }
 
-    write-text -type 'label' -text "Found $($wifiList.Count) Wi-Fi Connection settings stored on the system:"  -lineAfter
+    write-text -type 'label' -text "Found $($wifiList.Count) Wi-Fi Connection settings stored on the system:" -lineBefore -lineAfter
 
     foreach ($ssid in $wifiList) {
         try {
@@ -23,5 +23,7 @@ function get-wifi-creds {
         Write-Host "$password"
     }
    
+    Write-Host
     get-cscommand
 }
+
