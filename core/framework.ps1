3078f2f78be2323daca59960ce982461f3c704db
badsyntaxx <badsyntaxx@gmail.com>
2024-06-10 15:39:18 -0600

    updates

--- core/framework.ps1
+++ core/framework.ps1
@@ -122,17 +122,19 @@ function add-script {
 }
 
 function get-help() {
-    write-text -type "header" -text "DESCRIPTION:" -Color "DarkGray" -lineBefore
+    write-text -type "header" -text "DESCRIPTION:" -lineBefore
     write-text -type "plain" -text "Chased scripts aims to simplify tedious powershell commands and make common IT tasks" -Color "DarkGray"
     write-text -type "plain" -text "simpler by keeping commands logical, intuitive and short." -Color "DarkGray"
-    write-text -type "header" -text "DOCS:" -Color "DarkGray" -lineBefore 
+    write-text -type "header" -text "DOCS:" -lineBefore 
     write-text -type "plain" -text "https://chased.dev/chased-scripts" -Color "DarkGray"
-    write-text -type "header" -text "COMMANDS:" -Color "DarkGray" -lineBefore
+    write-text -type "header" -text "COMMANDS:" -lineBefore
     write-text -type "plain" -text "toggle admin                     - Toggle the Windows built-in administrator account." -Color "DarkGray"
     write-text -type "plain" -text "add [local,domain] user          - Add a local or domain user to the system." -Color "DarkGray"
     write-text -type "plain" -text "edit user [name,password,group]  - Edit user account settings." -Color "DarkGray"
     write-text -type "plain" -text "edit net adapter                 - Edit network adapter settings like IP and DNS." -Color "DarkGray"
     write-text -type "plain" -text "get wifi creds                   - View WiFi credentials saved on the system." -Color "DarkGray"
+    write-text -type "header" -text "PLUGINS:" -lineBefore
+    write-text -type "plain" -text "plugins [plugin name]  - Useful scripts made by other people. Try 'help plugins' for info about plugins." -Color "DarkGray"
     Write-Host
     Write-Host "    Skip entering more commands by entering the" -ForegroundColor "DarkGray" -NoNewLine
     Write-Host " menu" -ForegroundColor "Gray" -NoNewLine
