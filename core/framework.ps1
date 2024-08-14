72457610ef88fb4a17439f64d96d8d2ed29017ef
badsyntaxx <badsyntaxx@gmail.com>
2024-08-14 13:35:42 -0600

    Update

--- core/framework.ps1
+++ core/framework.ps1
@@ -178,7 +178,7 @@ function write-text {
             Write-Host "$text" -ForegroundColor "White" 
         }
         if ($type -eq "label") { Write-Host "    $text" -ForegroundColor "Yellow" }
-        if ($type -eq 'success') { Write-Host "    $text"  -ForegroundColor "Green" }
+        if ($type -eq 'success') { Write-Host "    -----$text-----"  -ForegroundColor "Green" }
         if ($type -eq 'error') { Write-Host "  X $text" -ForegroundColor "Red" }
         if ($type -eq 'notice') { Write-Host "    $text" -ForegroundColor "Yellow" }
         if ($type -eq 'plain') { Write-Host "    $text" -ForegroundColor $Color }
