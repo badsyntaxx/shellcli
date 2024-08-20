854bd5ca62e53b5921152a1118fd3b8c3ac93c45
badsyntaxx <badsyntaxx@gmail.com>
2024-08-20 16:03:54 -0600

    Update

--- core/framework.ps1
+++ core/framework.ps1
@@ -409,8 +409,9 @@ function read-option {
 
         for ($i = 0; $i -lt $options.Count; $i++) {
             Write-Host "  $(" " * ($longestKeyLength * $options[$orderedKeys[$pos]].Length))  "
-            Write-Host "  $(" " * ($longestKeyLength * $options[$orderedKeys[$pos]].Length))  "
         }
+        
+        Write-Host "  $(" " * ($longestKeyLength * $options[$orderedKeys[$pos]].Length))  "
         
         [Console]::SetCursorPosition($promptPos.X, $promptPos.Y)
         Write-Host
