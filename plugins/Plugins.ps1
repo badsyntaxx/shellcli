3998cdbe70edb709e38b2275c910ac72b1ab2604
badsyntaxx <badsyntaxx@gmail.com>
2024-08-29 21:17:07 -0600

    Update

--- plugins/Plugins.ps1
+++ plugins/Plugins.ps1
@@ -3,7 +3,7 @@ function read-menu {
         # Create a menu with options and descriptions using an ordered hashtable
         $choice = read-option -options $([ordered]@{
                 "massgravel"   = "https://github.com/massgravel/Microsoft-Activation-Scripts"
-                "reclaimw11"   = "https://gist.github.com/DanielLarsenNZ/edc6dd611418581ef90b02ad8e23b363#file-reclaim-windows-11-ps1"
+                "reclaimw11"   = "Unknown"
                 "win11debloat" = "https://github.com/Raphire/Win11Debloat"
                 "Cancel"       = "Select nothing and exit this menu."
             }) -prompt "Select a plugin:" -returnKey
