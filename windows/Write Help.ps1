4d3d677522ef2d2a3720c46f2c7c4174d473aac2
badsyntaxx <badsyntaxx@gmail.com>
2024-08-30 13:19:17 -0600

    uPDATE

--- windows/Write Help.ps1
+++ windows/Write Help.ps1
@@ -1,11 +1,8 @@
 function writeHelp {
-    writeText -type "plain" -text "COMMANDS:"
-    writeText -type "plain" -text "toggle admin                     - Toggle the Windows built-in administrator account." -Color "DarkGray"
-    writeText -type "plain" -text "add [local,domain] user          - Add a local or domain user to the system." -Color "DarkGray"
+    writeText -type "plain" -text "COMMAND STRUCTURE:" -lineBefore
+    writeText -type "plain" -text "add [local,ad] user              - Add a local or domain user to the system." -Color "DarkGray"
     writeText -type "plain" -text "edit user [name,password,group]  - Edit user account settings." -Color "DarkGray"
     writeText -type "plain" -text "edit net adapter                 - Edit network adapter settings like IP and DNS." -Color "DarkGray"
-    writeText -type "plain" -text "get wifi creds                   - View WiFi credentials saved on the system." -Color "DarkGray"
-    writeText -type "plain" -text "plugins [plugin name]            - Useful scripts made by others. Try the 'plugins help' command." -Color "DarkGray"
-    writeText -type "plain" -text "FULL DOCUMENTATION:" -lineBefore
+    writeText -type "plain" -text "FULL DOCUMENTATION:"
     writeText -type "plain" -text "https://guided.chaste.pro/dev/chaste-scripts" -Color "DarkGray"
 }
