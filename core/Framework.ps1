d1e93e680ac17d788eb5d5ba1790f04ed2d7f192
badsyntaxx <badsyntaxx@gmail.com>
2024-09-03 12:29:15 -0600

    Update

--- core/Framework.ps1
+++ core/Framework.ps1
@@ -451,7 +451,7 @@ function getDownload {
         [Parameter(Mandatory = $false)]
         [string]$ProgressText = 'Loading',
         [Parameter(Mandatory = $false)]
-        [string]$failText = 'Connection failed...',
+        [string]$failText = 'Download failed...',
         [parameter(Mandatory = $false)]
         [int]$MaxRetries = 2,
         [parameter(Mandatory = $false)]
@@ -460,7 +460,7 @@ function getDownload {
         [switch]$visible = $false
     )
     Begin {
-        function showProgress {
+        function Show-Progress {
             param (
                 [Parameter(Mandatory)]
                 [Single]$TotalValue,
@@ -490,13 +490,15 @@ function getDownload {
             $progbar = $progbar.PadRight($BarSize, [char]9617)
 
             if (!$Complete.IsPresent) {
-                Write-Host -NoNewLine "`r  $ProgressText $progbar $($percentComplete.ToString("##0.00").PadLeft(6))%"
+                Write-Host -NoNewLine "`r    $ProgressText $progbar $($percentComplete.ToString("##0.00").PadLeft(6))%"
             } else {
-                Write-Host -NoNewLine "`r  $ProgressText $progbar $($percentComplete.ToString("##0.00").PadLeft(6))%"                    
+                Write-Host -NoNewLine "`r    $ProgressText $progbar $($percentComplete.ToString("##0.00").PadLeft(6))%"                    
             }              
+             
         }
     }
     Process {
+        $downloadComplete = $true 
         for ($retryCount = 1; $retryCount -le $MaxRetries; $retryCount++) {
             try {
                 $storeEAP = $ErrorActionPreference
@@ -548,11 +550,11 @@ function getDownload {
           
                     if ($visible) {
                         if ($fullSize -gt 0) {
-                            showProgress -TotalValue $fullSizeMB -CurrentValue $totalMB -ProgressText $ProgressText -ValueSuffix "MB"
+                            Show-Progress -TotalValue $fullSizeMB -CurrentValue $totalMB -ProgressText $ProgressText -ValueSuffix "MB"
                         }
 
                         if ($total -eq $fullSize -and $count -eq 0 -and $finalBarCount -eq 0) {
-                            showProgress -TotalValue $fullSizeMB -CurrentValue $totalMB -ProgressText $ProgressText -ValueSuffix "MB" -Complete
+                            Show-Progress -TotalValue $fullSizeMB -CurrentValue $totalMB -ProgressText $ProgressText -ValueSuffix "MB" -Complete
                             $finalBarCount++
                         }
                     }
@@ -562,15 +564,23 @@ function getDownload {
                 if ($visible) {
                     Write-Host 
                 }
+                
+                if ($downloadComplete) { 
+                    return $true 
+                } else { 
+                    return $false 
+                }
             } catch {
-                # writeText -type "plain" -text "$($_.Exception.Message)"
-                writeText -type "plain" -text $failText
+                # write-text -type "fail" -text "$($_.Exception.Message)"
+                write-text -type "fail" -text $failText
+                
+                $downloadComplete = $false
             
                 if ($retryCount -lt $MaxRetries) {
-                    writeText "Retrying..."
+                    write-text "Retrying..."
                     Start-Sleep -Seconds $Interval
                 } else {
-                    writeText -type "error" -text "Load failed. Exiting function." 
+                    write-text -type "error" -text "Maximum retries reached." 
                 }
             } finally {
                 # cleanup
@@ -578,7 +588,8 @@ function getDownload {
                     $reader.Close() 
                 }
                 if ($writer) { 
-                    $writer.Flush(); $writer.Close() 
+                    $writer.Flush() 
+                    $writer.Close() 
                 }
         
                 $ErrorActionPreference = $storeEAP
