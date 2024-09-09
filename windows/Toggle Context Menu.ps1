ea71fd09bbd6aaa9fa607d4bbce8ffd0e175aafa
badsyntaxx <badsyntaxx@gmail.com>
2024-09-09 08:58:17 -0600

    hotfix

--- windows/Toggle Context Menu.ps1
+++ windows/Toggle Context Menu.ps1
@@ -7,8 +7,8 @@ function toggleContextMenu {
             }) -prompt "Would you like to enable or disable the W11 context menu?"
 
         switch ($choice) {
-            0 { enable-contextMenu }
-            1 { disable-contextMenu }
+            0 { enableContextMenu }
+            1 { disableContextMenu }
             2 { readCommand }
         }
 
@@ -18,14 +18,14 @@ function toggleContextMenu {
         writeText -type "error" -text "toggleAdmin-$($_.InvocationInfo.ScriptLineNumber) | $($_.Exception.Message)"
     }
 }
-function enablecontextMenu {
+function enableContextMenu {
     try {         
         reg delete "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f | Out-Null
     } catch {
         writeText -type "error" -text "enable-contextMenu-$($_.InvocationInfo.ScriptLineNumber) | $($_.Exception.Message)"
     }
 }
-function disablecontextMenu {
+function disableContextMenu {
     try {         
         reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve | Out-Null
     } catch {
