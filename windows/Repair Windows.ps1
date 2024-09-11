ee5160bea49d13b18a72a73716ecae4c21241bb4
badsyntaxx <badsyntaxx@gmail.com>
2024-09-11 11:56:57 -0600

    fgh

--- windows/Repair Windows.ps1
+++ windows/Repair Windows.ps1
@@ -1,7 +1,6 @@
 function repairWindows {
     try {
         $choice = readOption -options $([ordered]@{
-                "Run all"                = "Send it!"
                 "System file check"      = "Scans for and repairs corrupted system files."
                 "Cleanup & restore"      = "Scans for and repairs the Windows image."
                 "Restart update service" = "Restart the Windows update service."
@@ -9,14 +8,9 @@ function repairWindows {
             }) -prompt "Select a user account type:"
 
         switch ($choice) {
-            0 { 
-                & "C:\Windows\System32\cmd.exe" /c sfc /scannow
-                & "C:\Windows\System32\cmd.exe" /c DISM /Online /Cleanup-Image /RestoreHealth
-                restartUpdateService
-            }
-            1 { & "C:\Windows\System32\cmd.exe" /c sfc /scannow } 
-            2 { & "C:\Windows\System32\cmd.exe" /c DISM /Online /Cleanup-Image /RestoreHealth } 
-            3 { restartUpdateService } 
+            0 { & "C:\Windows\System32\cmd.exe" /c sfc /scannow } 
+            1 { & "C:\Windows\System32\cmd.exe" /c DISM /Online /Cleanup-Image /RestoreHealth } 
+            2 { restartUpdateService } 
         }
     } catch {
         writeText -type "error" -text "repairWindows-$($_.InvocationInfo.ScriptLineNumber) | $($_.Exception.Message)"
