f50d74d5ace71670109424d1a23edd9f0135b927
badsyntaxx <badsyntaxx@gmail.com>
2024-11-18 10:39:29 -0700

    Update

--- windows/Remove User.ps1
+++ windows/Remove User.ps1
@@ -1,7 +1,9 @@
 function removeUser {
     try {
         $user = selectUser -prompt "Select an account to remove:"
-        $dir = (Get-CimInstance Win32_UserProfile -Filter "SID = '$((Get-LocalUser $user["Name"]).Sid)'").LocalPath
+        $userProfile = Get-CimInstance Win32_UserProfile -Filter "SID = '$((Get-LocalUser $user["Name"]).Sid)'"
+        $dir = $userProfile.LocalPath
+
         $choice = readOption -options $([ordered]@{
                 "Delete" = "Also delete the users data."
                 "Keep"   = "Do not delete the users data."
@@ -9,32 +11,32 @@ function removeUser {
             }) -prompt "Do you also want to delete the users data?"
 
         if ($choice -eq 2) {
-            readCommand
+            return
         }
 
         Remove-LocalUser -Name $user["Name"] | Out-Null
-        if ($choice -eq 0) { 
-            if ($null -ne $dir) { 
-                Remove-Item -Path $dir -Recurse -Force 
+
+        $response = "The user has been removed"
+        if ($choice -eq 0 -and $dir) { 
+            try {
+                Remove-Item -Path $dir -Recurse -Force -ErrorAction Stop
+                
+                # Verify profile folder deletion
+                $u = Get-LocalUser -Name $user["Name"] -ErrorAction SilentlyContinue
+                $profileStillExists = Get-CimInstance Win32_UserProfile -Filter "SID = '$((Get-LocalUser $user["Name"]).Sid)'" -ErrorAction SilentlyContinue
+
+                if (!$u -and $null -eq $profileStillExists) {
+                    $response += " as well as their data."
+                } else {
+                    writeText -type 'error' -text "Unable to delete user data for unknown reasons."
+                    $response += " but their data could not be fully deleted."
+                }
+            } catch {
+                writeText -type 'error' -text "Failed to delete user profile folder: $($_.Exception.Message)"
+                $response += " but their data could not be deleted."
             }
-        }
-
-        $response = ""
-
-        $u = Get-LocalUser -Name $user["Name"] -ErrorAction SilentlyContinue
-
-        if (!$u) {
-            $response = "The user has been removed "
-        } 
-
-        if ($null -eq $dir) { 
-            if ($choice -eq 0) { 
-                $response += "as well as their data."
-            } else {
-                $response += "but not their data."
-            }
-        } else {
-            writeText -type 'error' -text "Unable to delete user data for unknown reasons."
+        } elseif ($choice -eq 1) {
+            $response += " but not their data."
         }
 
         writeText -type 'success' -text $response
