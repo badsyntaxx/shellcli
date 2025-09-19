f0334c9197993f18f9d0f1f185cb3c8f78a56150
badsyntaxx <badsyntaxx@gmail.com>
2025-09-19 14:39:42 -0600

    Update

--- core/Framework.ps1
+++ core/Framework.ps1
@@ -193,13 +193,10 @@ function writeText {
 
         # Format output based on the specified Type
         if ($type -eq "header") {
-            $l = $([char]0x2500)
+            # $l = $([char]0x2500)
             Write-Host " $([char]0x2502)" -ForegroundColor "Gray"
-            Write-Host " $([char]0x2502)" -NoNewline -ForegroundColor "Gray"
-            Write-Host " # " -ForegroundColor "Cyan" -NoNewline
-            Write-Host "$text" -ForegroundColor "White" 
-            Write-Host " $([char]0x250C)" -NoNewline -ForegroundColor "Gray"
-            Write-host " $l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l$l" -ForegroundColor "Cyan"
+            Write-Host " $([char]0x251C)" -NoNewline -ForegroundColor "Gray"
+            Write-Host " $text" -ForegroundColor "White" 
         }
 
         if ($type -eq "prompt") {
