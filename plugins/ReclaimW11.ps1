ff9d2da78a9e0103722ca8add3a2795fdcfbcae0
badsyntaxx <badsyntaxx@gmail.com>
2025-09-19 14:26:33 -0600

    Update

--- plugins/ReclaimW11.ps1
+++ plugins/ReclaimW11.ps1
@@ -1,4 +1,4 @@
-function reclaim {
+function reclaimw11 {
     writeText "Debloating Windows"
 
     $tweaks = @(    
@@ -863,7 +863,7 @@ function DisablePasswordPolicy {
     writeText "Disabling password complexity and maximum age requirements..."
     $tmpfile = New-TemporaryFile
     secedit /export /cfg $tmpfile /quiet
-	(Get-Content $tmpfile).Replace("PasswordComplexity = 1", "PasswordComplexity = 0").Replace("MaximumPasswordAge = 42", "MaximumPasswordAge = -1") | Out-File $tmpfile
+    (Get-Content $tmpfile).Replace("PasswordComplexity = 1", "PasswordComplexity = 0").Replace("MaximumPasswordAge = 42", "MaximumPasswordAge = -1") | Out-File $tmpfile
     secedit /configure /db "$env:SYSTEMROOT\security\database\local.sdb" /cfg $tmpfile /areas SECURITYPOLICY | Out-Null
     Remove-Item -Path $tmpfile
 }
@@ -873,7 +873,7 @@ function EnablePasswordPolicy {
     writeText "Enabling password complexity and maximum age requirements..."
     $tmpfile = New-TemporaryFile
     secedit /export /cfg $tmpfile /quiet
-	(Get-Content $tmpfile).Replace("PasswordComplexity = 0", "PasswordComplexity = 1").Replace("MaximumPasswordAge = -1", "MaximumPasswordAge = 42") | Out-File $tmpfile
+    (Get-Content $tmpfile).Replace("PasswordComplexity = 0", "PasswordComplexity = 1").Replace("MaximumPasswordAge = -1", "MaximumPasswordAge = 42") | Out-File $tmpfile
     secedit /configure /db "$env:SYSTEMROOT\security\database\local.sdb" /cfg $tmpfile /areas SECURITYPOLICY | Out-Null
     Remove-Item -Path $tmpfile
 }
