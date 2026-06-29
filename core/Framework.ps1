4eb68f85f8af81be59bd1a9f480e897156945bf1
badsyntaxx <badsyntaxx@gmail.com>
2026-06-29 15:00:59 -0400

    Update

--- core/Framework.ps1
+++ core/Framework.ps1
@@ -130,77 +130,28 @@ function filterCommands {
         $matchingKey = $script:commandMap.Keys | Where-Object { $_ -eq $normalizedCommand }
         
         if ($matchingKey) {
-            # Return the command array
             return $script:commandMap[$matchingKey]
         } else {
-            # Command not found
+            # Check if it's a PowerShell/Windows command (like your old switch did)
+            if ($normalizedCommand -ne "help" -and $normalizedCommand -ne "" -and $normalizedCommand -match "^(?-i)(\w+(-\w+)*)") {
+                $cmdName = $matches[1]
+                if (Get-Command $cmdName -ErrorAction SilentlyContinue) {
+                    # It's a valid PowerShell command, execute it and return $null
+                    $output = Invoke-Expression -Command $command
+                    $output | Format-Table | Out-String | ForEach-Object { Write-Host $_ }
+                    return $null
+                }
+            }
+            
+            # Command not found in map and not a PowerShell command
+            Write-Host " $([char]0x251C)" -NoNewline -ForegroundColor "Gray"
+            Write-Host "  Unrecognized command `"$command`". Try" -NoNewline -ForegroundColor "White"
+            Write-Host " help" -ForegroundColor "Cyan" -NoNewline
+            Write-Host " or" -NoNewline -ForegroundColor "White"
+            Write-Host " menu" -NoNewline -ForegroundColor "Cyan"
+            Write-Host " to learn more." -ForegroundColor "White"
             return $null
         }
-        <# $commandArray = $()
-
-        switch ($command) {
-            "" { $commandArray = $("windows", "Helpers", "shellCLI") }
-            "help" { $commandArray = $("windows", "Helpers", "writeHelp") }
-            "menu" { $commandArray = $("windows", "Helpers", "readMenu") }
-            "toggle context menu" { $commandArray = $("windows", "Toggle Context Menu", "toggleContextMenu") }
-            "enable context menu" { $commandArray = $("windows", "Toggle Context Menu", "enableContextMenu") }
-            "disable context menu" { $commandArray = $("windows", "Toggle Context Menu", "disableContextMenu") }
-            "toggle admin" { $commandArray = $("windows", "Toggle Admin", "toggleAdmin") }
-            "enable admin" { $commandArray = $("windows", "Toggle Admin", "enableAdmin") }
-            "disable admin" { $commandArray = $("windows", "Toggle Admin", "disableAdmin") }
-            "list users" { $commandArray = $("windows", "User", "listUsers") }
-            "user menu" { $commandArray = $("windows", "User", "userMenu") }
-            "add user" { $commandArray = $("windows", "User", "addUser") }
-            "add local user" { $commandArray = $("windows", "User", "addLocalUser") }
-            "add ad user" { $commandArray = $("windows", "User", "addADUser") }
-            "add drive letter" { $commandArray = $("windows", "Add Drive Letter", "addDriveLetter") }
-            "remove user" { $commandArray = $("windows", "User", "removeUser") }
-            "edit hostname" { $commandArray = $("windows", "Edit Hostname", "editHostname") }
-            "edit description" { $commandArray = $("windows", "Edit Hostname", "editDescription") }
-            "edit user" { $commandArray = $("windows", "User", "editUser") }
-            "edit user name" { $commandArray = $("windows", "User", "editUserName") }
-            "edit user password" { $commandArray = $("windows", "User", "editUserPassword") }
-            "edit user group" { $commandArray = $("windows", "User", "editUserGroup") }
-            "edit net adapter" { $commandArray = $("windows", "Edit Net Adapter", "editNetAdapter") }
-            "get wifi creds" { $commandArray = $("windows", "Get Wifi Creds", "getWifiCreds") }
-            "get software" { $commandArray = $("windows", "Get Software", "getSoftware") }
-            "get windirstat" { $commandArray = $("windows", "Get Software", "getWinDirStat") }
-            "get revouninstaller" { $commandArray = $("windows", "Get Software", "getRevoUninstaller") }
-            "schedule task" { $commandArray = $("windows", "Schedule Task", "scheduleTask") }
-            "update windows" { $commandArray = $("windows", "Update Windows", "updateWindows") }
-            "clear temp files" { $commandArray = $("windows", "Repair Windows", "clearTempFiles") }
-            "repair windows" { $commandArray = $("windows", "Repair Windows", "repairWindows") }
-            "plugins" { $commandArray = $("plugins", "Helpers", "plugins") }
-            "plugins menu" { $commandArray = $("plugins", "Helpers", "readMenu") }
-            "plugins help" { $commandArray = $("plugins", "Helpers", "writeHelp") }
-            "plugins reclaimw11" { $commandArray = $("plugins", "ReclaimW11", "reclaimw11") }
-            "plugins massgravel" { $commandArray = $("plugins", "massgravel", "massgravel") }
-            "plugins win11debloat" { $commandArray = $("plugins", "win11Debloat", "win11Debloat") }
-            "share gpu with vm" { $commandArray = ("windows", "Share GPU with VM", "shareGPUWithVM") }
-            "copy host gpu drivers to vm" { $commandArray = ("windows", "Share GPU with VM", "copyHostGPUDriversToVM") }
-            "install host gpu drivers on vm" { $commandArray = ("windows", "Share GPU with VM", "installHostGPUDriversOnVM") }
-            "partition gpu" { $commandArray = ("windows", "Share GPU with VM", "partitionGPU") }
-            "generate encrypted password" { $commandArray = ("windows", "Generate Encrypted Password", "generateEncryptedPassword") }
-            "add premade account" { $commandArray = ("windows", "Add Premade Account", "addPremadeAccount") }
-            default { 
-                if ($command -ne "help" -and $command -ne "" -and $command -match "^(?-i)(\w+(-\w+)*)") {
-                    if (Get-command $matches[1] -ErrorAction SilentlyContinue) {
-                        $output = Invoke-Expression -Command $command 
-                        $output | Format-Table | Out-String | ForEach-Object { Write-Host $_ }
-                        readCommand
-                    }
-                }
-                Write-Host " $([char]0x251C)" -NoNewline -ForegroundColor "Gray"
-                Write-Host "  Unrecognized command `"$command`". Try" -NoNewline -ForegroundColor "White"
-                Write-Host " help" -ForegroundColor "Cyan" -NoNewline
-                Write-Host " or" -NoNewline -ForegroundColor "White"
-                Write-Host " menu" -NoNewline -ForegroundColor "Cyan"
-                Write-Host " to learn more." -ForegroundColor "White"
-                readCommand 
-            }
-        }
-
-        return $commandArray #>
     } catch {
         writeText -type "error" -text "filterCommands-$($_.InvocationInfo.ScriptLineNumber) | $($_.Exception.Message)"
     }
