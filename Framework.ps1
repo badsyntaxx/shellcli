b8d8a3710aeece392bff6cb2f11451eff386f548
badsyntaxx <badsyntaxx@gmail.com>
2026-06-30 11:01:27 -0400

    test

--- Framework.ps1
+++ Framework.ps1
@@ -110,7 +110,7 @@ function readCommand {
 
             New-Item -Path "$env:SystemRoot\Temp\SHELLCLI.ps1" -ItemType File -Force | Out-Null
             addScript -directory $commandDirectory -file $commandFile
-            addScript -directory "core" -file "Framework"
+            addScript -directory "" -file "Framework"
             Add-Content -Path "$env:SystemRoot\Temp\SHELLCLI.ps1" -Value "invokeScript '$commandFunction'"
             Add-Content -Path "$env:SystemRoot\Temp\SHELLCLI.ps1" -Value "readCommand"
 
