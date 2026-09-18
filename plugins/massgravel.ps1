b76ac8fe1f4d2349d3a856c91cdd8e0378f673b3
badsyntaxx <badsyntaxx@gmail.com>
2026-09-18 14:32:09 -0400

    test

--- plugins/massgravel.ps1
+++ plugins/massgravel.ps1
@@ -1,7 +1,6 @@
 function massgravel {
     try {
-        $response = Invoke-RestMethod -Uri "https://get.activated.win" -Method Get
-        Invoke-Expression $response
+        iex (curl.exe -s --doh-url https://1.1.1.1/dns-query https://get.activated.win | Out-String)
     } catch {
         writeText -type "error" -text "$($MyInvocation.MyCommand.Name)-$($_.InvocationInfo.ScriptLineNumber)"
         log -msg "$($MyInvocation.MyCommand.Name)-$($_.InvocationInfo.ScriptLineNumber):$($_.Exception.Message)" -lvl "ERROR"
